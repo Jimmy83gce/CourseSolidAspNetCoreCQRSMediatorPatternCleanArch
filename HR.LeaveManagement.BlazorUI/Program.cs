d0cb2c771be2edd3b4a7a13b89426cb9e4e14b19
Jimmy83gce <jakub.pyznar@gmail.com>
2025-03-18 14:04:04 +0100

    Próba jojścia do sekcji 8 (5/6 -> no 63- Setup Leave Type Management Service)

--- HR.LeaveManagement.BlazorUI/Program.cs
+++ HR.LeaveManagement.BlazorUI/Program.cs
@@ -1,12 +1,26 @@
-using HR.LeaveManagement.BlazorUI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using HR.LeaveManagement.BlazorUI;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services;
+using System.Reflection;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { 
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IClient, Client>
+    (client => client.BaseAddress = new Uri
+    ("http://localhost:7202"));
+
+builder.Services.AddScoped<ILeaveTypeService, LeaveTypeService>();
+builder.Services.AddScoped<ILeaveAllocationService, LeaveAllocationService>();
+builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 await builder.Build().RunAsync();
